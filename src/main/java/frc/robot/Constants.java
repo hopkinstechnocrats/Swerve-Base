72c290504ebac75a1493b39ad42e88c3d64dc8e1
AJAmelse <aj.amelse@hopkinsrobotics.org>
2025-07-08 20:41:34 -0500

    read docs and added a bit

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,20 @@
+package frc.robot;
+
+public final class Constants{
+
+    public static final class SwerveConstants{
+        public static final int k_frontLeftDriveCANID = 0;
+        public static final int k_frontRightDriveCANID = 1;
+        public static final int k_backLeftDriveCANID = 2;
+        public static final int k_backRightDriveCANID = 3;
+
+        public static final int k_frontLeftTurnCANID = 4;
+        public static final int k_frontRightTurnCANID = 5;
+        public static final int k_backLeftTurnCANID = 6;
+        public static final int k_backRightTurnCANID = 7;
+
+
+    }
+
+
+}
---
+++ src/main/java/frc/robot/swerve/SwerveModule.java
@@ -0,0 +1,16 @@
+package frc.robot.swerve;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class SwerveModule extends SubsystemBase{
+
+    TalonFX driveMotor;
+    TalonFX turnMotor;
+    
+    SwerveModule(){
+
+    }
+
+}
